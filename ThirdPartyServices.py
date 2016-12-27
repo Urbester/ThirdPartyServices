f8a8874e905e6f0bcc1a2cfdf5fd5d68218912f9
alizardo <snow.off.work@gmail.com>
2016-12-27 23:24:01 +0000

    Add routes according the route page in the wiki page of the project

--- ThirdPartyServices.py
+++ ThirdPartyServices.py
@@ -1,12 +1,11 @@
 from flask import Flask
+from routes import *
 
 app = Flask(__name__)
-
 
 @app.route('/v1/')
 def status():
     return 'Status: Online'
-
 
 
 if __name__ == '__main__':
---
+++ routes/accounts.py
@@ -0,0 +1,15 @@
+from ThirdPartyServices import app
+
+@app.route('/v1/account', methods=["GET"])
+def get_account_info():
+    return
+
+
+@app.route('/v1/account', methods=["POST"])
+def create_account():
+    return
+
+@app.route('/v1/account', methods=["DELETE"])
+def delete_account():
+    return
+
