626d8d1ec77a2649c27b65d490e3f23a4b5c9717
amaralDaniel <amaral94daniel@gmail.com>
2016-12-28 12:14:06 +0000

    Create event model
    Add gitignore

--- models/Event.py
+++ models/Event.py
@@ -1,14 +1,36 @@
 from application import db
+from datetime import date
 
 class Event(db.Model):
     __tablename__ = "Event"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(256))
-    # One to Many Example
+    startDate = db.Column(date)
+    endDate = db.Column(date)
+    local = db.Column(db.String(256))
+    # One to Many
     acceptedGuests = db.relationship('User')
+    # One to Many
+    rejectedInvitations = db.relationship('User')
+    # One to Many
+    pendingInvitations = db.relationship('User')
+    description = db.Column(db.String(256))
+    price = db.Column(db.Integer)
+    owner = db.Column(db.relationship('User'))
 
 
-    def __init__(self, title, acceptedGuests):
+
+    def __init__(self, title, startDate, endDate, local, acceptedGuests,
+                 rejectedInvitations, pendingInvitations, description, price, owner  ):
         self.title = title
+        self.startDate = startDate
+        self.endDate = endDate
+        self.local = local
         self.acceptedGuests = acceptedGuests
+        self.rejectedInvitations = rejectedInvitations
+        self.pendingInvitations = pendingInvitations
+        self.description = description
+        self.price = price
+        self.owner = owner
+
