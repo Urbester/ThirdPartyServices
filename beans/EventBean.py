ae91c2fb10e1112a4d9c930d5535e255b0f9b166
amaralDaniel <amaral94daniel@gmail.com>
2016-12-28 14:44:16 +0000

    Add id to Event model
    Create EventBean

--- beans/EventBean.py
+++ beans/EventBean.py
@@ -1,14 +1,67 @@
 class EventBean(object):
-    # TODO: Just an example
-    def create_event(self):
+    def new_event(self, title, startDate, endDate, local, acceptedGuests,
+                  rejectedInvitations, pendingInvitations, description, price, owner):
         try:
-            from models import User
+            from models import Event
             from application import db
-            u = User(name=name, accessToken=accessToken, email=email)
-            db.session.add(u)
+            event = Event(title=title, startDate=startDate, endDate=endDate,
+                          local=local, acceptedGuests=acceptedGuests,
+                          rejectedInvitations=rejectedInvitations,
+                          pendingInvitations=pendingInvitations,
+                          description=description, price=price, owner=owner)
+            db.session.add(event)
             db.session.commit()
-            self.result = "Account created."
+            self.result = "Event created."
             return True
-        except Exception as e:
-            self.update_account(name, accessToken, email)
+        except Exception as event:
+            self.update_event(title, startDate, endDate, local, acceptedGuests,
+                              rejectedInvitations, pendingInvitations, description, price, owner)
+            return False
+
+    def update_event(self, id, title, startDate, endDate, local, acceptedGuests,
+                     rejectedInvitations, pendingInvitations, description, price):
+        try:
+            from models import Event
+            from application import db
+            event = Event.query.filter_by(id=self.id).first()
+            event.title = title
+            event.startDate = startDate
+            event.endDate = endDate
+            event.local = local
+            event.acceptedGuests = acceptedGuests
+            event.rejectedInvitations = rejectedInvitations
+            event.pendingInvitations = pendingInvitations
+            event.description = description
+            event.price = price
+            db.session.commit()
+            self.result = "Event updated"
+        except Exception as exception:
+            self.result = "Update Error"
+            return False
+
+    def delete_event(self, id):
+        try:
+            from models import Event
+            from application import db
+            event = Event.query.filter_by(id=self.id).first()
+            db.session.delete(event)
+            db.session.commit()
+            self.result = "Event was deleted."
+            return True
+        except Exception as exception:
+            self.result = "Event doesn't exist."
+            return False
+
+    def get_event(self, id):
+        try:
+            from models import Event
+            event = Event.query.filter_by(id=self.id).first()
+            self.result = {"title": event.title, "startDate": event.startDate, "endDate": event.endDate,
+                           "local": event.local, "acceptedGuests": event.acceptedGuests,
+                           "rejectedInvitations": event.rejectedInvitations,
+                           "pendingInvitations": event.pendingInvitations,
+                           "description": event.description, "price": event.price, "owner": event.owner}
+            return True
+        except Exception as exception:
+            self.result = "Event doesn't exist."
             return False
